3fc0aee80addacc8b8269f04a6c10def6f27aff9
ExtraSeven-code <apexexe124@gmail.com>
2025-07-25 09:04:59 -0500

    pacman
    se comenzo con el pacman

---
+++ IA_04/Assets/scripts/movimento_pacman.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimento_pacman : MonoBehaviour
+{
+    Transform pacman_transform;
+    CharacterController characterController;
+    [SerializeField] private float horizontal;
+    [SerializeField] private float vertical;
+    [SerializeField] private int velocidad;
+    [SerializeField] private int velocidad_rotacion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        pacman_transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+        }
+
+        
+
+    }
+    private void FixedUpdate()
+    {
+        characterController.Move(new Vector3(horizontal, 0, vertical) * velocidad * Time.deltaTime);
+    }
+}
